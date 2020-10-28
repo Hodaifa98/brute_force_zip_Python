4d2d01ab6506751db8710cf07837a446ecfeee63
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-28 15:32:00 +0100

    Commented.

--- crack_archive.py
+++ crack_archive.py
@@ -1,29 +1,39 @@
 #Import required modules.
 import zipfile
-import rarfile
 
+#Length of password.
+pass_length = 4
+
+#A list of characters to attempt to combine.
 charlist = "abcdefghijklmnopqrstuvwxyz"
+
+#An empty array to fill with potential passwords.
 complete = []
 
-for current in range(4):
+for current in range(pass_length):
     #Getting all the characters.
     a = [i for i in charlist]
     for x in range(current):
         #Using list comprehension to build the combinations.
         a = [y + i for i in charlist for y in a]
+    #Add password to list.
     complete = complete + a
 
-z = zipfile.ZipFile("secret.zip", "r", zipfile.)
+#Open the zip file.
+z = zipfile.ZipFile("secret.zip", "r")
 
+#Hold number of tries it took to find the password.
 tries = 0
 
+#Iterate through the passwords in the complete array.
 for password in complete:
     try:
         tries += 1
+        #Set the password and extract all files from the zip archive.
         z.setpassword(password.encode("ascii"))
-        z.extract("password.txt")
+        z.extractAll()
         print(f"Password was found: {password}\nTries: {tries}")
         break
-    except Exception as ex:
-        print (ex)
-        break;
+    except:
+        #If the extraction failed, simply pass because the password was incorrect.
+        pass
